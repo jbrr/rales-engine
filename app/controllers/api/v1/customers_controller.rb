3f31d9a9c39b9dbe8de80711401afdb73ab3f0f8
Jeffrey Ruane <ruane.jb@gmail.com>
2015-11-02 19:04:56 -0700

    Add find action to customers. Figured out a way to make search find not case sensitive, but its ugly

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CustomersController < ApplicationController
   respond_to :json
+  before_action :capitalize_params
 
   def index
     respond_with Customer.all
@@ -10,6 +11,20 @@ class Api::V1::CustomersController < ApplicationController
   end
 
   def find
-    respond_with Customer.find_by()
+    respond_with Customer.find_by(customer_params)
+  end
+
+  private
+
+  def customer_params
+    params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
+  end
+
+  def capitalize_params
+    customer_params.values.each do |value|
+      if value.class == String
+        value.capitalize!
+      end
+    end
   end
 end

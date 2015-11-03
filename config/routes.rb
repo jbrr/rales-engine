1063bf216d80d1cee9d310b816b3a212055da45d
Jeffrey Ruane <ruane.jb@gmail.com>
2015-11-02 18:10:23 -0700

    Create collection route for customers find

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :customers, except: [:new, :edit], defaults: { format: :json }
+      resources :customers, only: [:index, :show], defaults: { format: :json } do
+        collection do
+          get "find"
+        end
+      end
       resources :merchants, except: [:new, :edit], defaults: { format: :json }
       resources :invoices, except: [:new, :edit], defaults: { format: :json }
       resources :invoice_items, except: [:new, :edit], defaults: { format: :json }

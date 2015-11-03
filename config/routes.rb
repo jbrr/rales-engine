7049fa466cff957ea1fb8289d71fe8537161869d
Jeffrey Ruane <ruane.jb@gmail.com>
2015-11-02 17:21:20 -0700

    Add routes and actions for invoice endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :customer, except: [:new, :edit], defaults: { format: :json }
       resources :merchant, except: [:new, :edit], defaults: { format: :json }
+      resources :invoice, except: [:new, :edit], defaults: { format: :json }
     end
   end
 end

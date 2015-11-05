0e57413c5c4a817dcb6e0216d7c17d49bd4c5c70
Jeffrey Ruane <ruane.jb@gmail.com>
2015-11-04 21:19:56 -0700

    Finish controller testing for business logic that I completed

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -5,7 +5,12 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
 
   def setup
     @item = Item.create(id: 1, merchant_id: 5, name: "stuff", unit_price: 4372)
-    @invoice_item = InvoiceItem.create(id: 4, item_id: 1)
+    @invoice_item = InvoiceItem.create(id: 4, item_id: 1, invoice_id: 4, quantity: 5)
+    InvoiceItem.create(id: 5, item_id: 1, invoice_id: 5, quantity: 8)
+    Invoice.create(id: 4, created_at: "Tue, 27 Mar 2012 14:54:09 UTC +00:00")
+    Invoice.create(id: 5, created_at: "Tue, 27 Mar 2012 14:54:09 UTC +00:00")
+    Transaction.create(id: 9, invoice_id: 4, result: "success")
+    Transaction.create(id: 10, invoice_id: 5, result: "success")
     @merchant = Merchant.create(id: 5)
   end
 
@@ -55,5 +60,13 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
 
     assert_response :success
     assert_equal merchant.id, json_response["id"]
+  end
+
+  test "#best_day" do
+    get :best_day, format: :json, id: item.id
+
+    assert_response :success
+    response = {"best_day"=>"2012-03-27T14:54:09.000Z"}
+    assert_equal response, json_response
   end
 end
